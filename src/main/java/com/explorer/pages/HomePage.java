1918aed872e2a2a6b330ef81f1be5d8e9790f021
issoupadrink <ohmyedd@gmail.com>
2020-08-18 23:45:48 +0100

    Initial page object model for the explorers home page

---
+++ src/main/java/com/explorer/pages/HomePage.java
@@ -0,0 +1,64 @@
+package com.explorer.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(className = "SimpleInput_input__1OLje")
+    private WebElement searchTextBox;
+
+    @FindBy(className = "Search_searchButtonIcon__lLLWk")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//a[@href='/en/browse-epochs']")
+    private WebElement showMoreEpochs;
+
+    @FindBy(xpath = "//a[@href='/en/browse-blocks']")
+    private WebElement showMoreBlocks;
+
+    @FindBy(className = "CircularProgress_insideCircle__2bJ_q")
+    private WebElement latestEpochIcon;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goTo() {
+        this.driver.get("https://explorer.cardano-testnet.iohkdev.io/en");
+        this.wait.until(ExpectedConditions.visibilityOf(this.searchButton));
+    }
+
+    public void setSearchBoxText(String value) {
+        this.searchTextBox.clear();
+        this.searchTextBox.sendKeys(value);
+    }
+
+    public void goToLatestEpoch() {
+        this.latestEpochIcon.click();
+        this.wait.until(ExpectedConditions.textToBePresentInElement(
+                driver.findElement(By.className("DividerWithTitle_title__3SVyG")),"Epoch Summary"));
+    }
+
+    public void showMoreEpochs() {
+        this.showMoreEpochs.click();
+        this.wait.until(ExpectedConditions.textToBePresentInElement(
+                driver.findElement(By.className("DividerWithTitle_title__3SVyG")),"Browse Epochs"));
+    }
+
+    public void showMoreBlocks() {
+        this.showMoreBlocks.click();
+        this.wait.until(ExpectedConditions.textToBePresentInElement(
+                driver.findElement(By.className("DividerWithTitle_title__3SVyG")),"Browse Blocks"));
+    }
+}
